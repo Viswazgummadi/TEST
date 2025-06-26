7869f4b301320ba1f7ea3698216714ba0208d175
Viswazgummadi <viswazgummadi@gmail.com>
2025-06-26 16:32:14 +0530

    Remove backend from main repo to add as submodule

--- backend/app/routes/general_routes.py
+++
@@ -1,8 +0,0 @@
-# backend/app/routes/general_routes.py
-from flask import Blueprint, jsonify
-
-general_bp = Blueprint('general_api', __name__) # Renamed for clarity
-
-@general_bp.route('/hello')
-def hello():
-    return jsonify(message="Hello from Refactored Reploit Backend!")
